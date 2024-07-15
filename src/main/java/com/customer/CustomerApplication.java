f50975c8b6a4925b910545c0fde325012c52d36e
yangxy <276695072@qq.com>
2024-07-15 15:04:12 +0800

    1

--- src/main/java/com/customer/CustomerApplication.java
+++ src/main/java/com/customer/CustomerApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -19,5 +22,8 @@ public class CustomerApplication
 	public static void main( String[] args )
     {
     	SpringApplication.run(CustomerApplication.class, args);
+    	log.info("\n----------------------------------------------------------\n\t" +
+                "客户系统启动成功! "+
+                "----------------------------------------------------------");
     }
 }
